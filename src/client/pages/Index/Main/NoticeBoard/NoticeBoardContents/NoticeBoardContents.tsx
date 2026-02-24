71d46b11b894a54062a8a6b48c4adcdc355b9f04
Cepoglazik <cpo.gla3ik@gmail.com>
2026-02-24 23:49:01 +0200

    Added BlockEntries (shared/)

--- src/client/pages/Index/Main/NoticeBoard/NoticeBoardContents/NoticeBoardContents.tsx
+++ src/client/pages/Index/Main/NoticeBoard/NoticeBoardContents/NoticeBoardContents.tsx
@@ -1,8 +1,28 @@
 import styles from "./NoticeBoardContents.module.scss";
+import BlockEntry from "@shared/HTMLElements/BlockEntry/BlockEntry";
 
 export default function NoticeBoardContents() {
   
   return (
-    <div className={styles["notice-board-contents"]}></div>
+    <div className={styles["notice-board-contents"]}>
+      <BlockEntry />
+      <BlockEntry />
+      <BlockEntry />
+      <BlockEntry />
+      <BlockEntry />
+      <BlockEntry />
+      <BlockEntry />
+      <BlockEntry />
+      <BlockEntry />
+      <BlockEntry />
+      <BlockEntry />
+      <BlockEntry />
+      <BlockEntry />
+      <BlockEntry />
+      <BlockEntry />
+      <BlockEntry />
+      <BlockEntry />
+      <BlockEntry />
+    </div>
   );
 }
---
+++ src/shared/HTMLElements/BlockEntry/BlockEntry.tsx
@@ -0,0 +1,8 @@
+import styles from "./BlockEntry.module.scss";
+
+export default function BlockEntry(/* params: index, name, time, tag */) {
+  
+  return (
+    <div className={styles["block-entry"]}></div>
+  );
+}
