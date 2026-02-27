4579e8e08bfef6ad884eb4ba2cfb8631a079ee8a
Cepoglazik <cpo.gla3ik@gmail.com>
2026-02-27 13:17:54 +0200

    Colors changed & made BoardPageControl position floating

---
+++ src/client/pages/Index/Main/BoardPageControl/BoardPageControl.tsx
@@ -0,0 +1,10 @@
+import styles from "./BoardPageControl.module.scss";
+
+export default function BoardPageControl() {
+  
+  return (
+    <div className={styles["board-page-control"]}>
+      
+    </div>
+  );
+}
--- src/client/pages/Index/Main/Main.tsx
+++ src/client/pages/Index/Main/Main.tsx
@@ -1,6 +1,8 @@
 import styles from "./Main.module.scss";
 import NoticeBoard from "./NoticeBoard/NoticeBoard";
 import noticeBoardStyles from "./NoticeBoard/NoticeBoard.module.scss";
+import boardPageControlStyles from "./BoardPageControl/BoardPageControl.module.scss";
+import BoardPageControl from "./BoardPageControl/BoardPageControl";
 import { useEffect, useState } from 'react';
 
 export default function Main() {
@@ -23,15 +25,36 @@ export default function Main() {
         }
     }
     handleResize();
+    
+    const mainElement: null | HTMLElement = document.querySelector(`.${styles.main}`);
+    const boardPageControlElement: null | HTMLElement = document.querySelector(`.${boardPageControlStyles["board-page-control"]}`);
+    
+    function fixBoardPageControlPos(overflowElement) {
+      const mainElementScrollLeft = Number(overflowElement.scrollLeft.toFixed(0));
+      const mainElementScrollTop = Number(overflowElement.scrollTop.toFixed(0));
+      boardPageControlElement.style.left = `${(window.innerWidth / 2) - (boardPageControlElement.getBoundingClientRect().width / 2) + mainElementScrollLeft}px`;
+      boardPageControlElement.style.top = `${10 + mainElementScrollTop}px`;
+    }
+    
+    if (mainElement) {
+      fixBoardPageControlPos(mainElement);
+    }
 
     window.addEventListener('resize', handleResize);
+    if (mainElement) {
+      mainElement.addEventListener('scroll', () => fixBoardPageControlPos(mainElement));
+    }
     return () => {
       window.removeEventListener('resize', handleResize);
+      if (mainElement) {
+        mainElement.removeEventListener('scroll', () => fixBoardPageControlPos(mainElement));
+      }
     }
   }, [count]);
   
   return (
     <main className={styles.main}>
+      <BoardPageControl />
       <NoticeBoard />
     </main>
   );
